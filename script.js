21ae28801142b7a8929945421b63ca86a89424cc
aurorelp <aleperff@gmail.com>
2025-03-21 11:08:31 +0100

    added 16 cards on the gameboard

---
+++ script.js
@@ -0,0 +1,44 @@
+const cards = [
+    'https://picsum.photos/id/237/100/100',
+    'https://picsum.photos/id/238/100/100',
+    'https://picsum.photos/id/239/100/100',
+    'https://picsum.photos/id/240/100/100',
+    'https://picsum.photos/id/241/100/100',
+    'https://picsum.photos/id/242/100/100',
+    'https://picsum.photos/id/243/100/100',
+    'https://picsum.photos/id/244/100/100',
+];
+
+const gameBoard = document.getElementById('game-board');
+
+function createCard(CardURL) {
+    // création de la carte
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.value = CardURL;
+
+    // ajout du contenu de la carte - renvoi vers l'url de l'image
+    const cardContent = document.createElement('img');
+    cardContent.classList.add('card-content');
+    cardContent.src = `${CardURL}`;
+
+    // ici on lie l'image à la carte
+    card.appendChild(cardContent);
+    return card;
+}
+
+function duplicateArray(arraySimple){
+    let arrayDouble = [];
+    arrayDouble.push(...arraySimple);
+    arrayDouble.push(...arraySimple);
+
+    return arrayDouble;
+}
+
+const allCards = duplicateArray(cards);
+
+allCards.forEach(card => {
+    const cardHtml = createCard(card);
+    gameBoard.appendChild(cardHtml);
+
+})
